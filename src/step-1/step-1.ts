1d6aee56a4565009b76b1d21691675e591c8be26
Ty Kowalewski <ty@lattice.com>
2023-02-20 12:06:35 -0600

    completed step 1

---
+++ src/step-1/step-1.ts
@@ -0,0 +1,53 @@
+export function tick(item: { name: string; daysRemaining: number; quality: number }): {
+  name: string;
+  daysRemaining: number;
+  quality: number;
+} {
+  if (item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert') {
+    if (item.quality > 0) {
+      if (item.name != 'Sulfuras, Hand of Ragnaros') {
+        item.quality = item.quality - 1;
+      }
+    }
+  } else {
+    if (item.quality < 50) {
+      item.quality = item.quality + 1;
+      if (item.name == 'Backstage passes to a TAFKAL80ETC concert') {
+        if (item.daysRemaining < 11) {
+          if (item.quality < 50) {
+            item.quality = item.quality + 1;
+          }
+        }
+        if (item.daysRemaining < 6) {
+          if (item.quality < 50) {
+            item.quality = item.quality + 1;
+          }
+        }
+      }
+    }
+  }
+
+  if (item.name != 'Sulfuras, Hand of Ragnaros') {
+    item.daysRemaining = item.daysRemaining - 1;
+  }
+
+  if (item.daysRemaining < 0) {
+    if (item.name != 'Aged Brie') {
+      if (item.name != 'Backstage passes to a TAFKAL80ETC concert') {
+        if (item.quality > 0) {
+          if (item.name != 'Sulfuras, Hand of Ragnaros') {
+            item.quality = item.quality - 1;
+          }
+        }
+      } else {
+        item.quality = item.quality - item.quality;
+      }
+    } else {
+      if (item.quality < 50) {
+        item.quality = item.quality + 1;
+      }
+    }
+  }
+
+  return item;
+}
